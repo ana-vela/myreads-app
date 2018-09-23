7c0f73a90882f2fdd5d3acea1cb49cfd95cf26b1
anavela <ana@anavela.net>
2018-09-23 11:21:03 -0600

    creating main, search, and book components

---
+++ src/Main.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import Book from './Book.js';
+
+class Main extends Component {
+render() {
+  return(
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Currently Reading</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  <li>
+                    <Book />
+                  </li>
+                </ol>
+              </div>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Want to Read</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  <li>
+                    <Book />
+                  </li>
+                </ol>
+              </div>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Read</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  <li>
+                    <Book />
+                  </li>
+                </ol>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="open-search">
+          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+        </div>
+      </div>
+  );
+}
+}
+
+export default Main;
