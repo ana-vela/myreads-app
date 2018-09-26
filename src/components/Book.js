dd12dd5084b934a738e102ee1ae4142895b5bbc8
anavela <ana@anavela.net>
2018-09-25 19:05:44 -0600

    deleting unused lines

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import * as BooksAPI from './BooksAPI';
-
 
 class Book extends Component {
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import Book from './Book';
 import Shelf from './Shelf';
 import * as BooksAPI from './BooksAPI';
 
