f1e9bfef3e081d19432ffcccb476af32feb2ebc1
anavela <ana@anavela.net>
2018-09-25 19:05:13 -0600

    adding resources

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,8 @@
+
+ // Grateful for guidance from these two walk throughs https://www.youtube.com/watch?v=i6L2jLHV9j8
+//and https://www.youtube.com/watch?v=acJHkd6K5kI&=&feature=youtu.be
+
+
 import React from 'react'
 import { Route } from 'react-router-dom';
 import Search from './Search';
